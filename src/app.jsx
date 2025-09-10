e1913e792b584419b496c958d7fa2c9e649e155d
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-10 02:14:05 -0700

    Add pizza images and update Vite proxy config
    Added image support to the Pizza component and updated App.jsx to pass image props for each pizza. Updated index.html to include a stylesheet and changed the root div content. Modified vite.config.js to proxy /api and /public routes to the backend server.

--- src/app.jsx
+++ src/app.jsx
@@ -6,9 +6,9 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza - Order Now</h1>
-      <Pizza name="miki" description="like's hunburger" />
-      <Pizza name="kid" description="like's pizza" />
-      <Pizza name="my kid" description="like's egg" />
+      <Pizza name="miki" description="like's hunburger" image="/public/pizzas/pepperoni.webp" />
+      <Pizza name="kid" description="like's pizza" image="/public/pizzas/hawaiian.webp"/>
+      <Pizza name="my kid" description="like's egg" image="/public/pizzas/big_meat.webp"/>
     </div>
   );
 };
