74b3f140f64b7db1576d3570e2806e03308dcbcb
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-10 12:07:39 -0700

    Add Order component and update App to use it
    Introduced a new Order component for creating pizza orders, including pizza type and size selection. Updated App component to render Order instead of individual Pizza components.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from "react-dom/client"; // Correct import
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza - Order Now</h1>
-      <Pizza name="miki" description="like's hunburger" image="/public/pizzas/pepperoni.webp" />
-      <Pizza name="kid" description="like's pizza" image="/public/pizzas/hawaiian.webp"/>
-      <Pizza name="my kid" description="like's egg" image="/public/pizzas/big_meat.webp"/>
+     <Order/>
     </div>
   );
 };
