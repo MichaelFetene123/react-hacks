0dd70636a66cfddd1b2785f8575d23b4297b731f
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-12 03:55:02 -0700

    Add useDebugValue to usePizzaOfTheDay hook
    Introduced useDebugValue to provide better debugging information for the custom hook. Also improved code formatting and consistency.

--- src/usePizzaOfTheDay.js
+++ src/usePizzaOfTheDay.js
@@ -1,17 +1,21 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from "react";
 
-export  const usePizzaOfTheDay = () =>{ 
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay
+      ? `${pizzaOfTheDay.id}`
+      : "loading... "
+    );
 
-    const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const pizzaJson = await response.json();
+      setPizzaOfTheDay(pizzaJson);
+    }
+    fetchPizzaOfTheDay();
+  }, []);
 
-    useEffect(() => { 
-        async function fetchPizzaOfTheDay() {
-            const response = await fetch('/api/pizza-of-the-day');
-            const pizzaJson = await response.json();
-            setPizzaOfTheDay(pizzaJson);
-        }
-        fetchPizzaOfTheDay();
-    }, []);
-
-    return pizzaOfTheDay;
-}
+  return pizzaOfTheDay;
+};
