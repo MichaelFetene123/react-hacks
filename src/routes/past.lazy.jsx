eb917f3b0d86645962d3a989d82e3fc5dff24c4e
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-14 16:05:58 -0700

    Refactor imports in past.lazy.jsx
    Added imports for useState, useQuery, and getPastOrders to prepare for future implementation in the PastOrdersRoute component.

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -1,9 +1,10 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import getPastOrders from "../api/getPastOrders.js";
 
 export const Route = createLazyFileRoute("/past")({
   component: () => PastOrdersRoute,
 });
 
-function PastOrdersRoute() { 
-     
-}
+function PastOrdersRoute() {}
