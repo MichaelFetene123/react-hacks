35945ab9b8b14619bd82474a63371ae88b5a2ced
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-14 14:26:51 -0700

    Add /past route and integrate React Query
    Introduces a new lazy-loaded /past route and updates the route tree accordingly. Adds @tanstack/react-query and @tanstack/react-query-devtools dependencies, integrates ReactQueryDevtools into the root layout, and enables recommended ESLint rules for TanStack Query. Also updates ESLint and package configuration to support these changes.

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PizzaOfTheDay from "./../PizzaOfTheDay";
 import Header from "./../Header";
 import { CartContext } from "./../Contexts";
@@ -12,13 +14,17 @@ export const Route = createRootRoute({
   component: () => {
     const cartHook = useState([]);
     return (
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Outlet />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+        <ReactQueryDevtools />
+      </>
     );
   },
   notFoundComponent: NotFound, // Add this line
