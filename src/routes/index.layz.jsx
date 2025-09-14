c06d58aabeab8c862ad4be51ebdf5720044c312e
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-14 02:27:30 -0700

    Add index.layz route and generate route tree
    Introduces a new '/index/layz' route with a basic component and updates the auto-generated routeTree.gen.ts to include the new route. The index.layz page provides navigation links to 'Order' and 'Past Order' pages.

---
+++ src/routes/index.layz.jsx
@@ -0,0 +1,23 @@
+import { createFileRoute, Link } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/index/layz')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className='index'>
+          <div className='index-brand'>
+              <h1>Padre Gino's</h1>
+              <p>Pizza & Art at a location near you</p>
+          </div>
+          <ul>
+              <li>
+                    <Link to='/order'>Order</Link>
+              </li> <li>
+                    <Link to='/past'>Past Order</Link>
+              </li> 
+          </ul>
+    </div>
+  )
+}
