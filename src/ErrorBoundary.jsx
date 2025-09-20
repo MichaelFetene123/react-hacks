537932fdeab5c0ccc7f1118e7a16d693595c659a
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-20 04:20:12 -0700

    Refactor ErrorBoundary and add EBWithHooks component
    Refactored formatting and method definitions in ErrorBoundary for consistency. Added a new EBWithHooks component that uses the usePizzaOfTheDay hook and passes its value as a prop to ErrorBoundary.

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
 import { Link } from "@tanstack/react-router";
+import { usePizzaOfTheDay } from "./usePizzaOfTheDay";
 
 class ErrorBoundary extends Component {
-    state = { hasError: false };
-    constructor(props) {
-        super(props);
-        this.celebrateError = this.celebrateError.bind(this); 
-        
+  state = { hasError: false };
+  constructor(props) {
+    super(props);
+    this.celebrateError = this.celebrateError.bind(this);
   }
   static getDerivedStateFromError() {
     return { hasError: true };
@@ -14,27 +14,32 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     // send to TrackJS or Sentry
     console.error("ErrorBoundary caught some stupid error", error, info);
-    }
-    componentDidMount() { }
-    componentWillUnmount() { }
-    componentDidUpdate() { }
-celebrateError = () => {
-    this.setState({ celebration: "lol" });
   }
+  componentDidMount() {}
+  componentWillUnmount() {}
+  componentDidUpdate() {}
+  celebrateError = () => {
+    this.setState({ celebration: "lol" });
+  };
 
-    render() {
-      console.log(this.props.lol )
+  render() {
+    console.log(this.props.lol);
     if (this.state.hasError) {
       return (
         <div className="error-boundary">
           <h2>Uh oh!</h2>
           <p>
-            There was an error with this page. <Link to="/">click here</Link> to go back to home page. 
+            There was an error with this page. <Link to="/">click here</Link> to
+            go back to home page.
           </p>
         </div>
       );
-      }
-      return this.props.children;
+    }
+    return this.props.children;
   }
+}
+function EBWithHooks() {
+  const potd = usePizzaOfTheDay();
+  return <ErrorBoundary lol={potd} />;
 }
 export default ErrorBoundary;
