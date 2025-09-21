155866d3846f4cd09cd6507a7c2a4c3c9793ce86
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-21 09:38:22 -0700

    Add test for contact form submission
    Introduces a test to verify that the contact form can be submitted successfully. Mocks the fetch response and simulates user input and button click to ensure the form behaves as expected.

--- src/__tests__/contact.lazy.test.jsx
+++ src/__tests__/contact.lazy.test.jsx
@@ -3,3 +3,35 @@ import { expect, test, vi } from "vitest";
 import createFetchMock from "vitest-fetch-mock";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Route } from "../routes/contact.lazy";
+import { name } from "./../../node_modules/vitest/dist/chunks/creator.CBPphXqR";
+
+const queryClient = new QueryClient();
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+test("can submit contact form", async () => {
+  fetchMocker.mockResponse(JSON.stringify({ status: "ok" }));
+  const screen = render(
+    <QueryClientProvider queryClient={queryClient}>
+      <Route.options.component />
+    </QueryClientProvider>
+  );
+
+  const nameInput = screen.getAllByPlaceholderText("Name");
+  const emailInput = screen.getByPlaceholderText("Email");
+  const msgTextArea = screen.getByPlaceholderText("Message");
+
+  const testData = {
+    name: " Brian",
+    email: " brian@gmail.com",
+    message: "hello there",
+  };
+
+  nameInput.value = testData.name;
+  emailInput.value = testData.email;
+  msgTextArea.value = testData.message;
+
+  const btn = screen.getByRole("button");
+  btn.click();
+});
