6780d0c9fce83a21aaa5628b4fd0b3cb8e1e81cf
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-23 15:52:19 -0700

    Refactor usePizzaOfTheDay hook tests
    Removed unused getPizzaOfTheDay helper and added a test to verify API call returns the pizza of the day using renderHook and waitFor.

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -15,21 +15,14 @@ const testPizza = {
   size: { S: 12.25, M: 16.25, L: 20.25 },
 };
 
-function getPizzaOfTheDay() {
-  let pizza;
-
-  function TestComponent() {
-    pizza = usePizzaOfTheDay();
-    return null;
-  }
-
-  render(<TestComponent />);
-
-  return pizza;
-}
-
 test("gives null when first called", async () => {
   fetch.mockResponseOnce(JSON.stringify(testPizza));
   const { result } = renderHook(() => usePizzaOfTheDay());
   expect(result.current).toBeNull();
+});
+
+test("to call the API and give back the pizza of the day", async () => {
+     fetch.mockResponseOnce(JSON.stringify(testPizza));
+     const { result } = renderHook(() => usePizzaOfTheDay());
+     await  waitFor(() => expect(result.current).toEqual(testPizza));
 });
