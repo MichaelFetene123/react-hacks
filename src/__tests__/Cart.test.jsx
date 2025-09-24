e5f8682bec91e667203e53a37c8e3d60b515f101
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-24 03:28:13 -0700

    Remove redundant snapshot test from Cart.test.jsx
    Deleted an unnecessary test labeled 'lol' that was snapshotting a hardcoded pizza object. This cleans up the test file and removes unused code.

--- src/__tests__/Cart.test.jsx
+++ src/__tests__/Cart.test.jsx
@@ -7,13 +7,3 @@ test("snapshot with nothing in cart ", () => {
   expect(asFragment()).toMatchSnapshot();
 });
 
-test("lol", () => {
-    expect({
-        id: "calabrese",
-        name: "The Calabrese Pizza",
-        category: "Supreme",
-        Description: "lol pizza from Calabria",
-        image: "/pizzas/pizzas/calabrese.webp",
-        size: { S: 12.25, M: 16.25, L: 20.25 },
-    }).toMatchSnapshot();
-});
