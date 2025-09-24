de0ba716c59e857c1b37d67ce060caa27160147d
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-24 03:22:14 -0700

    Add snapshot test for Calabrese pizza object
    Introduced a new test case 'lol' in Cart.test.jsx to snapshot a Calabrese pizza object. Updated the corresponding snapshot file with the expected output.

--- src/__tests__/Cart.test.jsx
+++ src/__tests__/Cart.test.jsx
@@ -3,6 +3,17 @@ import { render } from "@testing-library/react";
 import Cart from "./../Cart";
 
 test("snapshot with nothing in cart ", () => {
-    const { asFragment } = render(<Cart cart={[]} />);
-    expect(asFragment()).toMatchSnapshot();
- })
+  const { asFragment } = render(<Cart cart={[]} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("lol", () => {
+    expect({
+        id: "calabrese",
+        name: "The Calabrese Pizza",
+        category: "Supreme",
+        Description: "lol pizza from Calabria",
+        image: "/pizzas/pizzas/calabrese.webp",
+        size: { S: 12.25, M: 16.25, L: 20.25 },
+    }).toMatchSnapshot();
+});
